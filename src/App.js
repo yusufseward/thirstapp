6fcb40ed03bb3dfeb5d1b3a6c38a88f6da46ad5a
yusufseward <yusufseward@gmail.com>
2023-01-23 13:25:53 -0600

    setting up tailwind

--- src/App.js
+++ src/App.js
@@ -3,35 +3,41 @@ import { useState } from 'react';
 
 function DonationForm({ handleSubmit, handleChange, amount }) {
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
+    <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg">
+      <label className="block font-medium text-gray-700 mb-2">
         Amount:
-        <input
-          type="number"
-          min="0.50"
-          step="0.50"
-          value={amount}
-          onChange={handleChange}
-        />
       </label>
-      <button type="submit">Donate</button>
+      <input
+        type="number"
+        min="0.50"
+        step="0.50"
+        value={amount}
+        onChange={handleChange}
+        className="form-input rounded-md"
+      />
+      <button type="submit" className="bg-indigo-500 text-white rounded-md py-2 px-4 mt-4">
+        Donate
+      </button>
     </form>
   );
 }
 
 function CharityList({ charities, handleSelect }) {
   return (
-    <ul>
+    <ul className="mt-6">
       {charities.map(charity => (
-        <li key={charity.id}>
+        <li key={charity.id} className="flex items-center mb-2">
           <input
             type="radio"
             name="charity"
             id={charity.id}
             value={charity.id}
             onChange={handleSelect}
+            className="form-radio"
           />
-          <label htmlFor={charity.id}>{charity.name}</label>
+          <label htmlFor={charity.id} className="ml-2 text-gray-700">
+            {charity.name}
+          </label>
         </li>
       ))}
     </ul>
@@ -61,8 +67,8 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Donate</h1>
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-medium mb-6">Donate</h1>
       <DonationForm
         handleSubmit={handleSubmit}
         handleChange={handleChange}
@@ -78,4 +84,3 @@ function App() {
 }
 
 export default App;
-
