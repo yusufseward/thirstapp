c85c85bf15863b5f2c6317e51360110a0be57680
yusufseward <yusufseward@gmail.com>
2023-01-23 13:05:53 -0600

    updated for simple base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+function DonationForm({ handleSubmit, handleChange, amount }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Amount:
+        <input
+          type="number"
+          min="0.50"
+          step="0.50"
+          value={amount}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Donate</button>
+    </form>
+  );
+}
+
+function CharityList({ charities, handleSelect }) {
+  return (
+    <ul>
+      {charities.map(charity => (
+        <li key={charity.id}>
+          <input
+            type="radio"
+            name="charity"
+            id={charity.id}
+            value={charity.id}
+            onChange={handleSelect}
+          />
+          <label htmlFor={charity.id}>{charity.name}</label>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
+  const [amount, setAmount] = useState('');
+  const [selectedCharityId, setSelectedCharityId] = useState('');
+  const charities = [
+    { id: '1', name: 'Charity A' },
+    { id: '2', name: 'Charity B' },
+    { id: '3', name: 'Charity C' },
+  ];
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Code to handle the submission of the donation form, such as sending the donation to the selected charity
+  }
+
+  function handleChange(event) {
+    setAmount(event.target.value);
+  }
+
+  function handleSelect(event) {
+    setSelectedCharityId(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Donate</h1>
+      <DonationForm
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        amount={amount}
+      />
+      <CharityList
+        charities={charities}
+        handleSelect={handleSelect}
+        selectedCharityId={selectedCharityId}
+      />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Navbar } from "react-bootstrap";
+
+function NavBar () {
+    return (
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">THIRST</span>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
