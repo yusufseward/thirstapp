a066a29fa5a7cf43fbc58bc5d1633b9c28a22e8a
yusufseward <yusufseward@gmail.com>
2023-01-23 17:55:53 -0600

    donationboxes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Topbar from './components/Navbar';
+
+function CharityBox({ charity }) {
+  return (
+    <div className={`bg-${charity.color} rounded-md p-4 flex-shrink-0`}>
+      <div className="text-sm">
+        <h2 className="font-medium text-gray-700">{charity.name}</h2>
+        <p className="text-gray-600">{charity.description}</p>
+      </div>
+    </div>
+  );
+}
+
+function CharityList({ charities }) {
+  return (
+    <div className="flex flex-wrap -mx-4">
+      {charities.map(charity => (
+        <CharityBox key={charity.id} charity={charity} />
+      ))}
+    </div>
+  );
+}
 
 function DonationForm({ handleSubmit, handleChange, amount }) {
   return (
@@ -23,64 +43,32 @@ function DonationForm({ handleSubmit, handleChange, amount }) {
   );
 }
 
-function CharityList({ charities, handleSelect }) {
-  return (
-    <ul className="mt-6">
-      {charities.map(charity => (
-        <li key={charity.id} className="flex items-center mb-2">
-          <input
-            type="radio"
-            name="charity"
-            id={charity.id}
-            value={charity.id}
-            onChange={handleSelect}
-            className="form-radio"
-          />
-          <label htmlFor={charity.id} className="ml-2 text-gray-700">
-            {charity.name}
-          </label>
-        </li>
-      ))}
-    </ul>
-  );
-}
-
 function App() {
   const [amount, setAmount] = useState('');
-  const [selectedCharityId, setSelectedCharityId] = useState('');
   const charities = [
-    { id: '1', name: 'Charity A' },
-    { id: '2', name: 'Charity B' },
-    { id: '3', name: 'Charity C' },
+    { id: '1', name: 'Charity A', color: 'red', description: 'Charity A is a great charity that does a lot of good for people in need' },
+    { id: '2', name: 'Charity B', color: 'green', description: 'Charity B is a great charity that does a lot of good for people in need' },
+    { id: '3', name: 'Charity C', color: 'blue', description: 'Charity C is a great charity that does a lot of good for people in need' },
   ];
 
   function handleSubmit(event) {
     event.preventDefault();
     // Code to handle the submission of the donation form, such as sending the donation to the selected charity
+    console.log(`Donating $${amount} to selected charity`)
   }
 
   function handleChange(event) {
     setAmount(event.target.value);
   }
 
-  function handleSelect(event) {
-    setSelectedCharityId(event.target.value);
-  }
-
   return (
-    <div className="container mx-auto">
-      <Topbar/>
-      <h1 className="text-3xl font-medium mb-6">Donate</h1>
-      <DonationForm
-        handleSubmit={handleSubmit}
-        handleChange={handleChange}
-        amount={amount}
-      />
-      <CharityList
-        charities={charities}
-        handleSelect={handleSelect}
-        selectedCharityId={selectedCharityId}
-      />
+    <div>
+      <Topbar />
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-medium mb-6">Donate</h1>    
+        <DonationForm handleSubmit={handleSubmit} handleChange={handleChange} amount={amount} />
+        <CharityList charities={charities} />
+      </div>
     </div>
   );
 }
