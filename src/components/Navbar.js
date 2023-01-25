a95507019a8d41b76535e06a90d062c1364caf49
yusufseward <yusufseward@gmail.com>
2023-01-25 14:34:19 -0600

    changed navbar color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 function Topbar() {
     return (
-      <nav className="fixed top-0 left-0 right-0 z-10 bg-sky-400 border-b">
+      <nav className="fixed top-0 left-0 right-0 z-10 bg-sky-500 border-b">
         <div className="container mx-auto px-4 py-3 flex items-center">
           <h1 className="text-xl font-medium text-white">THIRST</h1>
         </div>
